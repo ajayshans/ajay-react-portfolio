2ebf049aa5a9984e959b34e5663a90715cf14878
Ajay S <ajaycshan@gmail.com>
2024-01-21 20:37:30 +1100

    Resume.jsx page styling

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,29 @@
+import cv from '../assets/Ajay_Shan_cv.pdf'
+
+export default function Resume() {
+    return (
+        <div className="container p-5">
+        <h1>Resume</h1>
+        <p>Download my <a href={cv} download="Ajay_shan_cv.pdf">CV</a></p>
+        <h2>Front-end Skills</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Tailwind CSS</li>
+        </ul>
+        <h2>Back-end Skills</h2>
+        <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequalize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>GraphQL</li>
+        </ul>
+      </div>
+    );
+  }
+  
